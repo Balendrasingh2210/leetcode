1d6a9ce956e333afbec9c5125552d5f3e2fefdbf
Balendra Singh <balendrasingh310813@gmail.com>
2023-10-02 05:04:57 +0530

    Time: 0 ms (100%), Space: 6.3 MB (16.03%) - LeetHub

---
+++ 0390-elimination-game/0390-elimination-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lastRemaining(int n) {
+        int start = 1, diff = 1, no = n;
+        bool l2r = true;
+
+        while(no>1){
+            if(l2r || no%2 ==1){
+                start+=diff;
+            }
+            diff*=2;
+            no/=2;
+            l2r = !l2r;
+        }
+        return start;
+    }
+};
